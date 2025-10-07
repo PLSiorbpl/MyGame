ecb69e6a8209697d462721fd9e6e2d882db9c1f3
PLSiorbpl <pragnm@gmail.com>
2025-10-07 18:21:03 +0200

    Poprawki Menu

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
-#include <Windows.h>
+#include <Windows.h> // system()
 #include <string>
-#include <conio.h>
+#include <conio.h> // _getch() _kbhit() (GetChar) (KeyboardHit)
 #include <stdlib.h>
-#define WHITE FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE
-#define GRAY !FOREGROUND_RED | !FOREGROUND_GREEN | !FOREGROUND_BLUE
-#define YELLOW FOREGROUND_RED | FOREGROUND_GREEN
-#define CYAN FOREGROUND_GREEN | FOREGROUND_BLUE
+
+// Można by użyć Makr (#define WHITE FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE)
+int WHITE = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+int CYAN = FOREGROUND_GREEN | FOREGROUND_BLUE;
+int YELLOW = FOREGROUND_RED | FOREGROUND_GREEN;
+
+//using namespace std; // nie lubie tego wole std:: poprostu
 
 HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-int Start() {
+int Menu() {
     int Pozycja = 0;
     while (true) {
         SetConsoleTextAttribute(hOut, CYAN | FOREGROUND_INTENSITY);
         std::cout << "/-------------------------------------------------------------------------------\\\n";
-        SetConsoleTextAttribute(hOut, YELLOW);
+
+        SetConsoleTextAttribute(hOut, YELLOW | FOREGROUND_INTENSITY);
         std::cout << R"(|
 |        /$$      /$$            /$$$$$$                                   
 |       | $$$    /$$$           /$$__  $$                                  
@@ -28,21 +32,28 @@ int Start() {
 |                     /$$  | $$                                            
 |                    |  $$$$$$/                                            
 |                     \______/)" << "\n";
+
         SetConsoleTextAttribute(hOut, CYAN | FOREGROUND_INTENSITY);
         std::cout << "|-------------------------------------\\_/---------------------------------------|\n";
+
         SetConsoleTextAttribute(hOut, WHITE | FOREGROUND_INTENSITY);
         std::cout << "| Witaj w MyGame co chcesz zrobic?     |\n|                                      |\n";
-        SetConsoleTextAttribute(hOut, ((Pozycja == 0) ? WHITE : GRAY));
-        std::cout << "| Nowa Gra " << ((Pozycja == 0) ? "   <-                       |" : "                            |") << "\n";
-        SetConsoleTextAttribute(hOut, ((Pozycja == 1) ? WHITE : GRAY));
-        std::cout << "| Kontynouj" << ((Pozycja == 1) ? "   <-                       |" : "                            |") << "\n";
-        SetConsoleTextAttribute(hOut, ((Pozycja == 2) ? WHITE : GRAY));
-        std::cout << "| Wyjscie  " << ((Pozycja == 2) ? "   <-                       |" : "                            |") << "\n";
+
+        // Można użyć std::cout << "| Nowa Gra " << ((Pozycja == 0) ? "   <-                       |" : "                            |") << "\n";
+        // ale nie wiem czy można tak (jest to skrucony if (?) else (:))
+        if (Pozycja == 0) {std::cout <<"| Nowa Gra    "<<"   <--                   |\n";}
+        else {std::cout <<"| Nowa Gra    "<<"                         |\n";}
+        if (Pozycja == 1) {std::cout <<"| Wczytaj Gre "<<"   <--                   |\n";}
+        else {std::cout <<"| Wczytaj Gre "<<"                         |\n";}
+        if (Pozycja == 2) {std::cout <<"| Wyjscie     "<<"   <--                   |\n";}
+        else {std::cout <<"| Wyjscie     "<<"                         |\n";}
+
         SetConsoleTextAttribute(hOut, CYAN | FOREGROUND_INTENSITY);
         std::cout << "\\-------------------------------------/-\\---------------------------------------/\n";
 
+        // Klawiatura
         while (true) {
-            if (_kbhit()) {
+            if (_kbhit()) { // jak tego nie mozna to zrobie poprostu cin ale wtedy to -1000000 chinskich social credit
                 char key = _getch();
                 if (key == 'w' && Pozycja > 0) {
                     Pozycja -= 1;
@@ -56,17 +67,27 @@ int Start() {
             }
         }
 
-        system("cls"); // czyszczenie ekranu
+        // czyszczenie ekranu
+        system("cls");
     }
 }
 
 int main() {
-    int start = Start();
-    if (start == 2) {
-        SetConsoleTextAttribute(hOut, FOREGROUND_GREEN);
+    int start = Menu();
+    if (start == 2) { // Wyjscie
+        SetConsoleTextAttribute(hOut, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
         std::cout << "Gra Pomyslnie zamknieta\n";
-        return 0; // wyjście
-    } else if (start == 0) {
-        // Nowa Gra
+        system("pause");
+        return 0;
+    } else if (start == 0) { // Nowa Gra
+        SetConsoleTextAttribute(hOut, YELLOW | FOREGROUND_INTENSITY);
+        std::cout << "Nowa Gra WIP\n";
+        system("pause");
+        return 0;
+    } else if (start == 1) { // Wczytaj gre i zacznij
+        SetConsoleTextAttribute(hOut, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+        std::cout << "Wczytywanie WIP\n";
+        system("pause");
+        return 0;
     }
 }
